6a4f9c2309f9e46c29d44aa6c18c9af80c6763f0
Eduardo Ochoa <eduardo8af1@gmail.com>
2015-04-16 19:52:14 -0500

    Chat Layout created

--- app/src/main/java/com/cuantium/queondapp/ChatActivity.java
+++ app/src/main/java/com/cuantium/queondapp/ChatActivity.java
@@ -79,9 +79,6 @@ Boolean group_chat = false;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
